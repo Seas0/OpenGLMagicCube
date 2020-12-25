875fcdeae7e156655070637bdeafa35c40848191
Seas0 <seashkey@gmail.com>
2020-12-25 13:19:22 +0800

    add: basic phong light system add: blinn-phong specular light add: document
    change: model vertex array now include normal vector

--- resource/shader/fragmentShader.glsl
+++ resource/shader/fragmentShader.glsl
@@ -6,22 +6,57 @@ out vec4 FragColor;
 // TexCoord: texture coordinate
 in vec2 TexCoord;
 
-// PointColor: color of the point 
-in vec3 PointColor;
+in vec3 FragPos;
+
+in vec3 Normal;
 
 // texture samplers
 uniform sampler2D Texture;
 
 // lighting system
-uniform vec3 ambient;
+uniform vec3 lightPos; 
+uniform vec3 viewPos; 
+uniform vec3 lightColor;
 
 // color mask
 uniform vec3 mask;
 
+
+
 void main() {
-  // apply the mask on the texture
+  
+  // basic phong light system
+  // ------------------------
+  
+  // ambient
+  float ambientStrength = 0.01;
+  vec3 ambient = ambientStrength * lightColor;
+
+  // diffuse 
+  vec3 norm = normalize(Normal);
+  vec3 lightDir = normalize(lightPos - FragPos);
+  float diff = max(dot(norm, lightDir), 0.0);
+  vec3 diffuse = diff * lightColor;
+
+  // specular(Blinn-Phong)
+  float specularStrength = 0.5;
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 halfwayDir = normalize(lightDir + viewDir);
+  float spec = pow(max(dot(Normal, halfwayDir), 0.0), 32);
+  vec3 specular = lightColor * spec; 
+  
+  /*
+  // specular(Phong)
+  float specularStrength = 0.5;
+  vec3 viewDir = normalize(viewPos - FragPos);
+  vec3 reflectDir = reflect(-lightDir, norm);  
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+  vec3 specular = specularStrength * spec * lightColor;  
+  */
+
+  // apply mask & light on the texture
   FragColor =
       texture(Texture, TexCoord)
-      * vec4(ambient, 1.0f)
+      * vec4((ambient + diffuse + specular), 1.0f)
       * vec4(vec3(1.0f, 1.0f, 1.0f) - mask, 1.0f);
 }
--- resource/shader/vertexShader.glsl
+++ resource/shader/vertexShader.glsl
@@ -4,20 +4,24 @@
 // ----------------------------------------------------
 layout(location = 0) in vec3 aPosition;
 
+// aNormal: the normal vector of a vertex
+layout (location = 1) in vec3 aNormal;
+
 // aColor: the origin position of a vertex in local space
 // ----------------------------------------------------
-layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec3 aColor;
 
 // aTexCoord: the uv position of a vertex
 // --------------------------------------
-layout(location = 2) in vec2 aTexCoord;
+layout(location = 3) in vec2 aTexCoord;
 
 // TexCoord: the uv position of a vertex
 // -------------------------------------
 out vec2 TexCoord;
 
-// PointColor: the color of this point
-out vec3 PointColor;
+out vec3 FragPos;
+
+out vec3 Normal;
 
 // model: transform local space to world space
 uniform mat4 model;
@@ -29,6 +33,8 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
+  FragPos = vec3(model * vec4(aPosition, 1.0));
+  Normal = mat3(transpose(inverse(model))) * aNormal;  
   gl_Position = projection * view * model * vec4(aPosition, 1.0f);
   TexCoord = vec2(aTexCoord.x, aTexCoord.y);
 }
